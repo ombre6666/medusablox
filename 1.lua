f9f31c440fe872c5d43be8fd2c76cd2178d6f593
ombre6666 <fernandodiky37@gmail.com>
2026-05-10 21:07:52 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,15 +1,12 @@
 local TeleportService = game:GetService("TeleportService")
 local player = game.Players.LocalPlayer
 
-local data = _G.SPOTIFY_SERVER_ID
+local jobId = _G.SPOTIFY_SERVER_ID
 
-if data and typeof(data) == "table" then
-    local placeId = data[1]
-    local jobId = data[2]
-
-    if placeId and jobId then
-        if game.PlaceId ~= placeId or game.JobId ~= jobId then
-            TeleportService:TeleportToPlaceInstance(placeId, jobId, player)
-        end
-    end
+if jobId and jobId ~= "" then
+    TeleportService:TeleportToPlaceInstance(
+        game.PlaceId, -- otomatis ambil dari game sekarang
+        jobId,
+        player
+    )
 end
